a6a5dae7fcc8ed344bcb5ce923efd44b97d5eb6a
seunex17 <seunexseun@gmail.com>
2021-02-14 19:23:18 +0100

    Composer modification

--- example/index.php
+++ example/index.php
@@ -4,7 +4,7 @@ use Zubdev\Transaction;
 
 require_once './zubdev/Transaction.php';
 
-$secretKey = "FLWSECK_TEST-eee25be1b44ef9a132a872075b3a0910-X";
+$secretKey = "YOUR SEC KEY";
 $publicKey = "";
 
 $txn = new Transaction($secretKey, $publicKey);
